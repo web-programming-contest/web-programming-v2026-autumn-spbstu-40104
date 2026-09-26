716c414ee8fc208814ca76b8e4192ce177a4cd8c
ilyaspbstu <ilyakabanov559@gmail.com>
2026-09-26 12:31:38 +0300

    format-fix

--- kabanov.ilya/lab3/solution.js
+++ kabanov.ilya/lab3/solution.js
@@ -1,6 +1,8 @@
 export function rotateArray(arr, steps) {
   const len = arr.length;
-  if (len === 0) return [];
-  const n = ((steps % len) + len) % len; // n for steps > len; ((steps % len) + len) % len for negative steps
+  if (len === 0) {
+    return [];
+  }
+  const n = ((steps % len) + len) % len;
   return n === 0 ? arr.slice() : [...arr.slice(-n), ...arr.slice(0, len - n)];
 }
